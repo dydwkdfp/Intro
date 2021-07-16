ddc41e5dfcd58eba3f0b1fdb439f062d7d37b186
dydwkdfp <dydwkdfp@naver.com>
2021-07-16 15:27:13 +0900

    Arrow change

--- src/components/Projects/ImageSlider.js
+++ src/components/Projects/ImageSlider.js
@@ -1,7 +1,7 @@
 import { Hidden } from '@material-ui/core'
 import React,{useState} from 'react'
-import { FaArrowAltCircleRight } from 'react-icons/fa'
-import { FaArrowAltCircleLeft } from 'react-icons/fa'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 
 
@@ -24,8 +24,8 @@ const ImageSlider = ({slides}) => {
 
     return (
         <section className="slider">
-            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
-            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
+            <ArrowBackIosIcon className="left-arrow" onClick={prevSlide}/>
+            <ArrowForwardIosIcon className="right-arrow" onClick={nextSlide}/>
             {slides.map((slide, index) => {
                     return(
                         <div className={index === current ? 'slide active' : 'slide'} key={index}>
